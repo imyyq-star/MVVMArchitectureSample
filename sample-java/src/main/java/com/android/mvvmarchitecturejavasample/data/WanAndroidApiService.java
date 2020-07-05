392c20e614aca6a2afc929bb3c58116fad042fd0
imyyq-mac <imyyq.star@gmail.com>
2020-07-05 18:50:56 +0800

    增加 Retrofit 的 Rx 和原生支持，统一封装请求结果和状态码还有错误信息。

--- sample-java/src/main/java/com/android/mvvmarchitecturejavasample/data/WanAndroidApiService.java
+++ sample-java/src/main/java/com/android/mvvmarchitecturejavasample/data/WanAndroidApiService.java
@@ -1,11 +1,17 @@
 package com.android.mvvmarchitecturejavasample.data;
 
+import com.android.mvvmarchitecturejavasample.entity.BaseEntity;
 import com.android.mvvmarchitecturejavasample.entity.DemoEntity;
 
+import java.util.List;
+
 import io.reactivex.Observable;
+import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface WanAndroidApiService {
     @GET("wxarticle/chapters/json")
-    Observable<DemoEntity> demoGet();
+    Observable<BaseEntity<List<DemoEntity>>> demoGet();
+    @GET("https://www.google.com")
+    Call<BaseEntity<List<DemoEntity>>> demoGet2();
 }
